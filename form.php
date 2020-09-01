359b5761a99fbdac09032368748db80b18ff0b8a
farah <farahaiman548@gmail.com>
2020-09-01 19:22:32 +0500

    comment

---
+++ form.php
@@ -0,0 +1,21 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$massage =$_POST['message'];
+
+$email_from = 'farahaiman548@gmail.com';
+
+$email_subject = " New Form Submission";
+
+$email_body = "User Name: $name.\n". 
+                "User Email: $visitor_email.\n". 
+                   "User Message: $massage.\n";
+
+
+$to = "007sahirkhan@gmail.com";
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-to: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact-php.html");                
+?>
